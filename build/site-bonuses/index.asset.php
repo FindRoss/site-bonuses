f92fe072aa49a2633ca1d176d0633ba4afe2220b
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-15 18:56:09 +0100

    Remove build directory from repository

--- build/site-bonuses/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '8453eed2186d1ea58713');
--- build/site-bonuses/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '32a02b2e39cd02cb3d69');
